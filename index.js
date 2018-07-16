9c16a38c92cae7529f51615dc4ff74f06ebb7506
nawar yossef <nawaryossef2@gmail.com>
2018-07-16 15:16:01 -0500

    updated code

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.use(
 
 app.get("/api", (req, res) => res.send("server is OK"))
 
+app.get("/api/users", (req, res) => {
+  const users = [
+    {name: "Ray"},
+    {name: "James"},
+  ]
+  res.json(users)
+})
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
